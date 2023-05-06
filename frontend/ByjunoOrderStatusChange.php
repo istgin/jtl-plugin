a3b700d20a5a8e4853850f64833ecd2ce0224191
byjuno <byjuno-dev>
2023-05-06 11:03:36 +0300

    .

--- frontend/ByjunoOrderStatusChange.php
+++ frontend/ByjunoOrderStatusChange.php
@@ -43,6 +43,7 @@
 
 use JTL\Plugin\Helper as PluginHelper;
 use JTL\Shop;
+use JTL\Catalog\Currency;
 use Plugin\byjuno\paymentmethod\ByjunoBase;
 
 $byjunoPlugin  = PluginHelper::getPluginById(ByjunoBase::PLUGIN_ID);
@@ -50,20 +51,19 @@ if ($byjunoPlugin === null) {
     exit('Byjuno payment plugin cant be found.');
 }
 $byjunoConfig = $byjunoPlugin->getConfig();
-
 try {
     $arr = isset($args_arr) ? $args_arr : [];
     if (!empty($arr) && is_array($arr) && !empty($arr["oBestellung"])) {
         $order = $arr["oBestellung"];
         if (!empty($order->kBestellung)) {
-            $byjunoOrder = Shop::Container()->getDB()->select('xplugin_byjyno_orders', ['order_id', 'request_type'], [$order->kBestellung, 'S3']);
+            $byjunoOrder = Shop::Container()->getDB()->select('xplugin_byjyno_orders', ['order_id', 'request_type'], [$order->cBestellNr, 'S3']);
             if (!empty($byjunoOrder) && $byjunoOrder->request_type == 'S3') {
                 if ($order->cStatus != $byjunoOrder->order_status) {
                     if ($byjunoConfig->getOption("byjuno_s4")->value == "true") {
                         $s4TriggerStatus = byjunoOrderMapStatus($byjunoConfig->getOption("byjuno_s4_trigger")->value);
                         if (!empty($s4TriggerStatus) && $s4TriggerStatus == $order->cStatus) {
-                            $invoiceNum = $order->kBestellung;
-                            $currency = $order->Waehrung->getCode();
+                            $invoiceNum = $order->cBestellNr;
+                            $currency = new Currency($order->kWaehrung);
                             $amount = $order->fGesamtsumme;
                             if (!empty($order->kKunde)) {
                                 $customerId = $order->kKunde;
@@ -72,7 +72,7 @@ try {
                             }
                             $time = time();
                             $dt = date("Y-m-d", $time);
-                            $requestInvoice = CreateShopRequestS4($invoiceNum, $amount, $amount, $currency, $invoiceNum, $customerId, $dt);
+                            $requestInvoice = CreateShopRequestS4($invoiceNum, $amount, $amount, $currency->getCode(), $invoiceNum, $customerId, $dt);
                             $xmlRequestS4 = $requestInvoice->createRequest();
                             $byjunoCommunicator = new ByjunoCommunicator();
                             $byjunoCommunicator->setServer("test");
@@ -86,13 +86,16 @@ try {
                                 $statusS4 = $byjunoResponseS4->getProcessingInfoClassification();
                             }
                             $byjunoLogger = ByjunoLogger::getInstance();
-                            $byjunoLogger->log(array(
-                                "firstname" => "-",
-                                "lastname" => "-",
-                                "town" => "-",
-                                "postcode" => "-",
-                                "street" => "-",
-                                "country" => "-",
+                            $byjunoLogger->addSOrderLog(Array(
+                                "order_id" => $order->cBestellNr,
+                                "order_status" => $order->cStatus,
+                                "request_type" => "S4",
+                                "firstname" => "",
+                                "lastname" =>  "",
+                                "town" => "",
+                                "postcode" =>  "",
+                                "street" => "",
+                                "country" =>  "",
                                 "ip" => byjunoGetClientIp(),
                                 "status" => $statusS4,
                                 "request_id" => $requestInvoice->getRequestId(),
@@ -124,3 +127,4 @@ try {
     }
 } catch (Exception $e) {
 }
+exit();
